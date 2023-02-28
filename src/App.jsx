e90e2cb2cd8b6388f8aedd883cb9ffff4fbd6934
Hashem <hashemabualteen@gmail.com>
2023-02-28 10:22:15 +0200

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Home</div>,
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
